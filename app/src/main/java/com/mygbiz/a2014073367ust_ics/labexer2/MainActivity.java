fdf2807c431731f55046c61c389b5fc63542da64
jamesearltan <jamesearl.tan.iics@ust.edu.ph>
2018-09-01 19:29:13 +0800

    Initial commit

---
+++ app/src/main/java/com/mygbiz/a2014073367ust_ics/labexer2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mygbiz.a2014073367ust_ics.labexer2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
